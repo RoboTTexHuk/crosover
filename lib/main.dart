9e52e20d11bd66a4c0728689dd6ebaabde2a96cf
RoboTTexHuk <migom@mail.ru>
2025-02-14 18:42:21 +0300

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
+import 'package:appsflyer_sdk/appsflyer_sdk.dart';
 import 'package:crossoverretro/Filltler.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -30,12 +32,62 @@ class WebViewPage extends StatefulWidget {
 class _WebViewPageState extends State<WebViewPage> {
   final GlobalKey webViewKey = GlobalKey();
   InAppWebViewController? webViewController;
+
+  String? iosSystemVersion;
+  String? deviceInformation;
+  String? appsFlyerUniqueId;
+  String? _firebaseCloudToken;
+  late AppsflyerSdk _customAppsFlyerSdk;
+  String  currentUrl="";
+  String advertisingId = "Fetching Advertising ID...";
+  String trackingStatus = "Unknown";
   bool isLoading = true; // Показывает, идет ли загрузка страницы
   final List<ContentBlocker> contentBlockers = [];
 
+
+  Future<void> _initializeATT() async {
+    final TrackingStatus status =
+    await AppTrackingTransparency.trackingAuthorizationStatus;
+
+    if (status == TrackingStatus.notDetermined) {
+      await Future.delayed(const Duration(milliseconds: 1000));
+      await AppTrackingTransparency.requestTrackingAuthorization();
+    }
+
+    final uuid = await AppTrackingTransparency.getAdvertisingIdentifier();
+    print("UUID: $uuid");
+  }
+  void _initializeAppsFlyer() {
+    AppsFlyerOptions options = AppsFlyerOptions(
+      afDevKey: "qsBLmy7dAXDQhowM8V3ca4",
+      appId: "6742021017",
+      showDebug: true,
+      timeToWaitForATTUserAuthorization: 30,
+    );
+
+    _customAppsFlyerSdk = AppsflyerSdk(options);
+
+    _customAppsFlyerSdk.initSdk(
+      registerConversionDataCallback: true,
+      registerOnAppOpenAttributionCallback: true,
+      registerOnDeepLinkingCallback: true,
+    );
+
+    _customAppsFlyerSdk.startSDK(
+      onSuccess: () {
+        print("AppsFlyer SDK успешно инициализирован.");
+      },
+      onError: (int errorCode, String errorMessage) {
+        print(
+            "Ошибка инициализации AppsFlyer SDK: Код $errorCode - $errorMessage");
+      },
+    );
+  }
   @override
   void initState() {
     super.initState();
+    _initializeATT();
+_initializeAppsFlyer();
 
 
     for (final adUrlFilter in Filltler) {
